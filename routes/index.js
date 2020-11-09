fa64f278a365601796e6e4c39f1bec4a43b5a7ee
A00886105 <95jvt71@gmail.com>
2020-11-08 16:13:31 -0800

    checkpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express woah...' });
+  // res.render('index', { title: 'Express woah...' });
+  res.redirect('/catalog');
 });
 
 module.exports = router;
