442b781ce130df95c18d16bb09c0ba31d26f7304
A00886105 <95jvt71@gmail.com>
2020-11-21 10:08:09 -0800

    clean up touch before deployment

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +22,9 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
+
+app.use(compression()); //Compress all routes
+app.use(helmet()); //HTTP headers that help protect your app from well-known web vulnerabilities
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
